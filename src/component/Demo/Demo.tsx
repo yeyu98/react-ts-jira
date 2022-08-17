3ef644a1a176e14bd14f60c8993ee86b0fc5240f
lzy-Jerry <763626908@qq.com>
2022-08-17 16:09:15 +0800

    feat: test prettier

---
+++ src/component/Demo/Demo.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+function Demo(props: any) {
+  const demo = () => {};
+  useEffect(() => {
+    demo();
+  });
+  return <div></div>;
+}
+
+export default Demo;
