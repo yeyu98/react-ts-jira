5c7873cd7cce9e7562d09ed542d0b3b47aa933e6
lzy-Jerry <763626908@qq.com>
2022-08-19 19:29:19 +0800

    feat: 删除无用代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ProjectListScreen from "screens/ProjectList/Index";
-import errorCaptured from "utils/errorCaptured";
 import "./App.css";
 
 function App() {
-  const asyncFunc = () => {
-    return new Promise((resolve, reject) => {
-      reject("218192781739812739");
-    });
-  };
-
-  const demo = async () => {
-    const [res, err] = await errorCaptured(asyncFunc);
-    console.log(res, err);
-  };
-
-  useEffect(() => {
-    demo();
-  });
-
   return (
     <div className="App">
       <ProjectListScreen />
