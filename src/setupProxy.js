c201f0eb12b84391482a897d4f323e2f2b351f82
lzy-Jerry <763626908@qq.com>
2023-04-22 16:57:01 +0800

    feat: 配置代理

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,12 +3,26 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     "/api",
+    // createProxyMiddleware({
+    //   target: "http://service.picasso.adesk.com/",
+    //   changeOrigin: true,
+    //   pathRewrite: {
+    //     "^/api": "", // 替换api为''
+    //   },
+    // }),
     createProxyMiddleware({
-      target: "http://service.picasso.adesk.com/",
+      target: "https://cat-match.easygame2021.com/",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "", // 替换api为''
       },
     })
+    // createProxyMiddleware({
+    //   target: "https://api.juejin.cn",
+    //   changeOrigin: true,
+    //   pathRewrite: {
+    //     "^/api": "", // 替换api为''
+    //   },
+    // })
   );
 };
